bf498e90f659aa1383d5aaaa38ad2fee5485e784
raduedu <eduradustefan1@gmail.com>
2025-09-04 23:20:18 +0300

    test 2 - 2021 - bun

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -10,50 +10,43 @@ b. Scrieți programul C/C++ corespunzător algoritmului proiectat.
 #include <fstream>
 using namespace std;
 
-int v[1000000];
-
 int main() {
 	ifstream fin("bac.in");
-	int nr, pp = -1, pn = 0, up = -1, un = 0, k = 1; //pozitii: px = primul x; ux = ultimul x; xp = al x-lea pozitiv; xn = al x-lea negativ. 
+	int nr, pp = -1, pn = 0, up = -1, un = 0, k = 0; //pozitii: px = primul x; ux = ultimul x; xp = al x-lea pozitiv; xn = al x-lea negativ. 
 	while (fin >> nr) {
-		v[k] = nr;
 		k++;
-	} k = k - 1; //bagam toate elementele in vector si pastram variabila care ne spune cate elemente sunt in total in vector
+		if (nr >= 0 && pp == -1)
+			pp = k;
+		if (nr < 0 && pn == 0)
+			pn = k;
+		if (nr >= 0)
+			up = k;
+		if (nr < 0)
+			un = k;
+	} 
+	//k = k - 1; //pastram variabila care ne spune cate elemente sunt in total in vector (nvm, nu mai avem vector)
 
-	//for (int i = 1; i <= k; i++)
-	//	cout << v[i] << " "; //afisam valorile din vector
 
-	for (int i = 1; i <= k; i++) {
-		if (v[i] >= 0 && pp == -1)
-			pp = i;
-		if (v[i] < 0 && pn == 0)
-			pn = i;
-		if (pp != -1 && pn != 0)
-			i = k+1;
-	}
+	
 	//cout << "\nprima val poz este pe pozitia: " << pp << endl;
 	//cout << "prima val NEGATIVA este pe pozitia: " << pn << endl;
-	for (int i = k; i >= 1; i--) {
-		if (v[i] >= 0 && up == -1)
-			up = i;
-		if (v[i] < 0 && un == 0)
-			un = i;
-		if (up != -1 && un != 0)
-			i = 0;
-	}
 	//cout << "ULTIMA val poz este pe pozitia: " << up << endl;
 	//cout << "ULTIMA val NEGATIVA este pe pozitia: " << un << endl;
-	if ((up - pn) + 1 >= (un - pp) + 1)
-		cout << (up - pn) + 1 << " [pn, up]";
-	else cout << (un - pp) + 1 << " [pp, un]";
-	cout << " este lungimea maxima";
+	
+	// ^- cu asta putem afisa chestii random useful ig
 
+	if ((up - pn) + 1 >= (un - pp) + 1)
+		cout << (up - pn) + 1;// << " [pn, up]";
+	else cout << (un - pp) + 1;// << " [pp, un]";
+	cout << " este lungimea maxima"; //cand am facut parea mai eficient pentru program, dupa putina analiza pot spune ca este mai eficient doar pentru cel care scrie programul, pentru a nu scrie acelasi lucru de 2 ori "in programare, daca scrii acelasi lucru de 2 ori faci ceva gresit"
 
 	fin.close();
 	return 0;
 }
 
 /*
-Programul este eficient din punct de vedere al memoriei, deoarece retine doar elementele din tabloul unidimensional si pe cele absolut necesare (primul/ultimul pozitiv/negativ + un contor pentru a stii cate elemente sunt in tablou)
-Programul este eficient din punct de vedere al timpului de executie, deoarece elementele sirului sunt complet parcurse o singura data, 
+Programul este eficient din punct de vedere al memoriei, deoarece retine doar ultima pozitie cautata din tabloul unidimensional si pozitia elementelor cautate, acestea fiind primul numar pozitiv, primul numar negativ, ultimul numar pozitiv si ultimul numar negativ.
+Programul este eficient din punct de vedere al timpului de executie, deoarece elementele sirului sunt parcurse o singura data.
+
+Programul verifica numerele din vector si vede daca indeplinesc conditiile pentru a fi unul dintre cele 4 numere cautate, acestea fiind primul numar pozitiv, primul numar negativ, ultimul numar pozitiv si ultimul numar negativ, daca da, le este stocata pozitia. Apoi, se scad pozitia ultimului numar pozitiv si pozitia primului numar negativ, cat si pozitia ultimului numar negativ si cea a primului numar pozitiv pentru a determina dimensiunile fiecarui sir. Dupa ce programul determina lungimile cautate, le comapara si ne afiseaza lungimea maxima cautata.
 */
