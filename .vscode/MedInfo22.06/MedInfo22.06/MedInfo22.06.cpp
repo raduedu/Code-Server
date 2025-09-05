a3a9e789bd9d4ecc1a59b0ee5cf256c20dbe82df
raduedu <eduradustefan1@gmail.com>
2025-09-05 14:38:18 +0300

    test 7 -2021

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -1,29 +1,27 @@
 /*
-Fişierul bac.txt conţine numere naturale din intervalul [2,106]: pe prima linie n, iar pe a doua linie un șir de n numere, separate prin câte un spațiu.
-Se cere să se afișeze pe ecran, pentru fiecare număr natural i (i aparinte [1, n]) , cea mai mare dintre primele i valori ale şirului aflat în fişier. 
-Numerele afișate sunt separate prin câte un spațiu. Proiectați un algoritm eficient din punctul de vedere al memoriei utilizate şi al timpului de executare.
-Exemplu: dacă fișierul are conținutul alăturat, se afișează pe ecran
-12
-4 6 3 7 8 1 6 2 7 9 10 8
-
-4 6 6 7 8 8 8 8 8 9 10 10
-
+Fişierul bac.txt conține cel mult 106 cifre, separate prin câte un spațiu.
+Se cere să se afișeze pe ecran, separate prin câte un spațiu, toate cifrele pare care apar în fișier sau mesajul nu exista, dacă nu există astfel de cifre. Proiectați un algoritm eficient din punctul de vedere al timpului de executare.
+Exemplu: dacă fișierul conține cifrele 3 3 0 8 2 1 2 1 3 7 1 5 2 7 1 0 3 2 3
+pe ecran se afişează, de exemplu în ordine crescătoare, cifrele 0 0 2 2 228
 a. Descrieți în limbaj natural algoritmul proiectat, justificând eficienţa acestuia.
-b. Scrieți programul C/C++ corespunzător algoritmului proiectat.
+b. Scrieți programul C/C++corespunzător algoritmului proiectat.
 */
 #include <iostream>
 #include <fstream>
 using namespace std;
 
+int v[10];
+
 int main() {
     ifstream fin("bac.in");
-    int nr, max = 1; //n si i sunt useless
-    fin >> nr;
+    int nr;
     while (fin >> nr) {
-        if (nr >= max) {
-            max = nr;
-        }
-        cout << max << " ";
+        v[nr]++;
+    }
+
+    for (int i = 0; i <= 8; i+=2) {
+        for (int j = 1; j <= v[i]; j++)
+            cout << i << " ";
     }
 
     fin.close();
@@ -31,9 +29,9 @@ int main() {
 }
 
 /*a)
-Programul este eficient din punct de vedere al memoriei, deoarece nu tine minte decat ultimul numar, si maximul intalnit in sir.
-Programul este eficient din punct de vedere al timpului de executie, deoarece are o singura bucla care verifica fiecare numar din sir si vede daca 
-acesta este maxim. Daca este maxim ne arata rezultatul, pana cand se termina se ajunge la acelasi numar de numere ca cel din sir.
+Programul este eficient din punct de vedere al memoriei, deoarece stocheaza doar numarul de cifre, dar nu cifrele in sine
+Programul este eficient din punct de vedere al timpului de executie, deoarece parcurge sirul de cifre o singura data, iar apoi, 
+in functie de vectorul caracteristic afisez valorile cautate de cate ori au aparut in sirul initial
 
-Programul verifica daca numerele de pe cel de al doilea rand din fisier sunt maxime, daca sunt, atunci le afiseaza, daca nu sunt, afiseaza ultima valoare maxima.
+Programul verifica de cate ori apare fiecare cifra si il stocheaza pe pozitia cifrei respective (ex: daca v[2] = 3, atunci 2 este afisat de 3 ori)
 */
