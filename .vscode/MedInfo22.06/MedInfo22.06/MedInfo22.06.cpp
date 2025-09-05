8688fa709aee8d8939ff9f193099cf95fffcdb9b
raduedu <eduradustefan1@gmail.com>
2025-09-05 14:50:46 +0300

    test 8 - 2021

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -1,37 +1,41 @@
 /*
-Fişierul bac.txt conține cel mult 106 cifre, separate prin câte un spațiu.
-Se cere să se afișeze pe ecran, separate prin câte un spațiu, toate cifrele pare care apar în fișier sau mesajul nu exista, dacă nu există astfel de cifre. Proiectați un algoritm eficient din punctul de vedere al timpului de executare.
-Exemplu: dacă fișierul conține cifrele 3 3 0 8 2 1 2 1 3 7 1 5 2 7 1 0 3 2 3
-pe ecran se afişează, de exemplu în ordine crescătoare, cifrele 0 0 2 2 228
+Fişierul bac.txt conține un șir de cel mult 10^6 numere naturale din intervalul [0,10^9].
+Se cere să se determine și să se afișeze pe ecran, separate printr-un spațiu, ultimele două numere impare (nu neapărat distincte) 
+din şirul aflat în fișier, sau mesajul nu exista, dacă nu există două astfel de numere. Proiectați un algoritm eficient din punctul 
+de vedere al memoriei utilizate și al timpului de executare.
+Exemplu: dacă fișierul conține valorile 122 1635 628 1413 1647 900 3001 4252
+se afișează pe ecran 1647 3001
 a. Descrieți în limbaj natural algoritmul proiectat, justificând eficienţa acestuia.
-b. Scrieți programul C/C++corespunzător algoritmului proiectat.
+b. Scrieți programul C/C++ corespunzător algoritmului proiectat.
 */
 #include <iostream>
 #include <fstream>
 using namespace std;
 
-int v[10];
-
 int main() {
     ifstream fin("bac.in");
-    int nr;
+    int n1 = 0, n2 = 0, nr;
     while (fin >> nr) {
-        v[nr]++;
+        if (nr % 2 == 1) {
+            n2 = n1;
+            n1 = nr;
+        }
     }
+    if (n1 * n2 == 0)
+        cout << "Nu exista\n";
+    else
+        cout << n2 << " " << n1;
 
-    for (int i = 0; i <= 8; i+=2) {
-        for (int j = 1; j <= v[i]; j++)
-            cout << i << " ";
-    }
 
     fin.close();
     return 0;
 }
 
 /*a)
-Programul este eficient din punct de vedere al memoriei, deoarece stocheaza doar numarul de cifre, dar nu cifrele in sine
-Programul este eficient din punct de vedere al timpului de executie, deoarece parcurge sirul de cifre o singura data, iar apoi, 
-in functie de vectorul caracteristic afisez valorile cautate de cate ori au aparut in sirul initial
+Programul este eficient din punct de vedere al memoriei, deoarece stocheaza doar ultimele 2 numere impare, nu prelucreaza tabloul unidmensional
+Programul este eficient din punct de vedere al timpului de executie, deoarece parcurge sirul de cifre o singura data, si determina care sunt 
+ultimele doua numere impare gasite
 
-Programul verifica de cate ori apare fiecare cifra si il stocheaza pe pozitia cifrei respective (ex: daca v[2] = 3, atunci 2 este afisat de 3 ori)
+Programul verifica fiecare numar si determina daca este impar. Daca da, atunci penultimul numar ia valoarea ultimului numar impar, 
+iar cele doua se afiseaza.
 */
