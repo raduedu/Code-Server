7ee6ad88201e8c21c730981cdfef54270058e125
raduedu <eduradustefan1@gmail.com>
2025-09-10 12:57:40 +0300

    test 11 - 2021 - III - a)? (pbn)

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -1,9 +1,10 @@
-/* test 11 2021 - III - 3 - a)
-Fişierul bac. txt conține un șir de cel mult 10^5 numere naturale din intervalul [1,10^9], separate prin câte un spaţiu.
-Se cere să se afişeze pe ecran cea mai mică poziţie pe care ar putea-o ocupa primul termen al şirului aflat în fişier în şirul format cu aceleași valori, ordonat crescător. 
-Proiectați un algoritm eficient din punctul de vedere al memoriei utilizate şi al timpului de executare.
-Exemplu: dacă fişierul conţine numerele 15 7 15 17 6 4
-se afişează pe ecran 4 (15 se află pe a patra și pe a cincea poziție în şirul 4, 6, 7, 15, 15, 17).
+/*
+Se consideră şirul 1, 3, 7, 13, 21, 31, 43 ... definit astfel: f_{0} = 1 iar f_{n} = f_{n - 1} + 2n, dacă n >= 1 (unde n este un număr natural). 
+Se citesc de la tastatură două numere naturale din intervalul [1,10^9], x și y (x < y), reprezentând doi termeni aflați pe poziții consecutive în şirul dat, și se cere să se scrie în fișierul text bac.out,
+separați prin câte un spațiu, toți termenii şirului mai mici sau egali cu y, în ordine inversă a apariției lor în şir.
+Proiectați un algoritm eficient din punctul de vedere al spațiului de memorie și al timpului de executare.
+Exemplu: dacă x = 21 și y = 31 , fişierul conţine valorile
+31 21 13 7 3 1
 a. Descrieți în limbaj natural algoritmul proiectat, justificând eficienţa acestuia.
 b. Scrieți programul C/C++ corespunzător algoritmului proiectat.
 */
@@ -12,30 +13,21 @@ b. Scrieți programul C/C++ corespunzător algoritmului proiectat.
 using namespace std;
 
 int main() {
-    ifstream fin("bac.in");
-    int cn = 0, k = 0, nr;
-
-    fin >> nr;
-    cn = nr;
-    while (fin >> nr) {
-        if (nr < cn)
-            k++;
+    ofstream fout("bac.out");
+    int x, y;
+    cin >> x >> y;
+    for (int i = (y - x)/2; i >= 0; i--) {
+        fout << y << " ";
+        y -= 2 * i;
     }
-    cout << k + 1;
 
-    cout << endl;
-    fin.close();
+    fout << endl;
+    fout.close();
     return 0;
 }
-
 /*a)
-Programul este eficient din punct de vedere al memoriei, deoarece stocheaza doar pozitia pe care s-ar afla numarul daca ar fi intr-un sir crescator, 
-o copie a primului numar din sir, si numarul curent verificat din sir
-Programul este eficient din punct de vedere al timpului de executie, deoarece parcurge sirul de cifre o singura data, simultan verificand daca numarul 
-curent din sir este mai mic decat primul numar, daca da, contorul creste.
+Programul este eficient din punct de vedere al memoriei, deoarece retine doar numerele x si y.
+Programul este eficient din punct de vedere al timpului de executie, deoarece, dupa ce i se ofera cifrele x si y incepe sa parcurga sirul de numere, in functie de formula oferita, in ordine inversa.
 
-Programul pastreaza o copie a primului numar din sir. Apoi, verifica celelalte numere din sir, daca acestea sunt mai mici decat primul numar, atunci 
-contorul nostru creste, contorul determina pozitia pe care ar lua-o primul numar in cazul unei sortari crescatoare. Totusi, o sortare nu este necesara, 
-pozitia sa poate fi determinata doar stiind cate numere ar fi inaintea acestuia. Apoi, se afiseaza contorul + 1, care este pozitia finala a primului numar, 
-in cazul unei sortari crescatoare.
+Programul foloseste formula din enunt si parcurge sirul in ordine inversa, de la f(n) pana la f(0).
 */
