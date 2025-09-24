4cac2cdccb74d80f3833eccb46e4cd36b95a441d
raduedu <eduradustefan1@gmail.com>
2025-09-24 15:43:38 +0300

    973 pbinfo

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -4,18 +4,28 @@
 using namespace std;
 
 int main() {
-	int n;
-	char c[256];
-	cin.getline(c,256);
-	n = strlen(c);
-	cout << (char)toupper(c[0]);
-	for (int i = 1; i <= n - 1; i++) {
-		if (c[i - 1] == ' ' || c[i + 1] == ' ' || c[i+1] == 0 || c[i - 1] == 0) //c[i+1] sau c[i-1] == 0 (null) este necesar? exista alta scriere mai buna?
-			cout << (char)toupper(c[i]); //daca nu pun (char) inainte imi converteste char-ul in valoare int ASCII
-		else cout << c[i];
-
+	int ok = 1, n = 0;
+	char c[256], *p, voc[11];
+	cin.getline(c, 256);
+	
+	strcpy(voc, "aeiouAEIOU");
+	
+	p = strtok(c, " ");
+	while (p != NULL) {
+		n = strlen(p);
+		ok = 1;
+		
+		for (int i = 0; i < n && ok == 1; i++) {
+			if (strchr(voc, p[i]) == NULL)
+				ok = 0;
+		}
+		
+		if (ok == 1)
+			cout << p << endl;
+			
+		p = strtok(NULL, " ");
 	}
 	
-	cout << endl;
+	cout<<endl;
 	return 0;
 }
