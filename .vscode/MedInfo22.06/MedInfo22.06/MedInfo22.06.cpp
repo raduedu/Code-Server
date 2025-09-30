5ee3c63b1d9eb58556e0ccea0281bf7c8533bdd5
Edu Radu-Ștefan <eduradustefan1@gmail.com>
2025-09-30 18:54:16 +0300

    test git vs

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -1,31 +1,13 @@
-#include <iostream> 
+#include <iostream>
 #include <cstring>
 #include <cctype>
 using namespace std;
-
 int main() {
-	int ok = 1, n = 0;
-	char c[256], *p, voc[11];
-	cin.getline(c, 256);
-	
-	strcpy(voc, "aeiouAEIOU");
-	
-	p = strtok(c, " ");
-	while (p != NULL) {
-		n = strlen(p);
-		ok = 1;
-		
-		for (int i = 0; i < n && ok == 1; i++) {
-			if (strchr(voc, p[i]) == NULL)
-				ok = 0;
-		}
-		
-		if (ok == 1)
-			cout << p << endl;
-			
-		p = strtok(NULL, " ");
-	}
-	
-	cout<<endl;
+	//hello there
+	char s[20];
+	strcpy(s, "stilou" + 4);
+	cout << s << endl;
+	strncpy(s, "stilou", 4);
+	cout << s << endl;
 	return 0;
 }
