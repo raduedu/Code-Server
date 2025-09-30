9665a5c34dbab2ad327211a867b47955a4f8f819
Edu Radu-Ștefan <eduradustefan1@gmail.com>
2025-09-30 18:44:36 +0300

    Add project files.

---
+++ MedInfo22.06/MedInfo22.06.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cstring>
+#include <cctype>
+using namespace std;
+
+int main() {
+    char s[20];
+    strcpy(s, "stilou" + 4);
+    cout << s << endl;
+    strncpy(s, "stilou", 4);
+    cout << s << endl;
+
+	return 0;
+}
