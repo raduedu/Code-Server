2a813c7f035fc59d62f3b9f8ac8f39fa90fb5358
Edu Radu-Ștefan <eduradustefan1@gmail.com>
2025-10-02 22:24:08 +0300

    test git vs

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -4,25 +4,18 @@
 using namespace std;
 
 int main() {
-	int n = 0, l = 0, lp = 0, ok = 0;
-	char prefix[21], s[500], *p;
+	int l = 0, lv = 0, ok = 0, k = 0;
+	char s[103], * p, c;
 
-	cin >> n;
-	strcpy(s, "");
-	for (int i = 0; i <= n; i++) {
-		cin.getline(prefix, 21);
-		if (strlen(s) > 0)
-			strcat(s, " ");
-		strcat(s, prefix);
-	}
-	lp = strlen(prefix);
+	cin.getline(s, 103);
+	l = strlen(s);
 	p = strtok(s, " ");
 
 	while (p != NULL) {
-		l = strlen(p);
-		if (l > lp && strncmp(p, prefix, lp) == 0) {
-			cout << p << " ";
-			ok = 1;
+		lv = strlen(p);
+		c = strlen(p) + strlen(strtok(p, " "));
+		if () {
+
 		}
 		p = strtok(NULL, " ");
 	}
@@ -33,20 +26,14 @@ int main() {
 	return 0;
 }
 /*
-Un cuvânt este prefix al unui alt cuvânt dacă se obține din acesta, prin eliminarea ultimelor sale litere.
-Scrieți un program C/C++ care citește de la tastatură un număr natural n (n apartine [2,20]) și apoi n cuvinte
-distincte, fiecare fiind format din cel mult 20 de caractere, numai litere mici ale alfabetului englez.
-La introducerea datelor, după fiecare cuvânt se tastează Enter. Programul afișează pe
-ecran, separate prin câte un spațiu, cuvintele care îl au drept prefix pe ultimul cuvânt citit.
-Dacă nu există astfel de cuvinte, se afișează pe ecran mesajul nu exista.
+Într-un text cu cel mult 102 caractere cuvintele sunt formate din litere mici ale alfabetului englez și sunt
+separate prin câte un spațiu. Scrieți un program C/C++ care citește de la tastatură un text de tipul
+menționat, pe care îl modifică în memorie, inserând cuvântul succes între oricare două cuvinte ale sale
+care se termină cu aceeași literă. Cuvântul inserat este separat prin câte un spațiu de cuvintele vecine.
+Textul transformat este afișat pe ecran, iar dacă nu există perechi de astfel de cuvinte, se afișează pe
+ecran mesajul *nu exista*.
 
-Exemplu: dacă n=6 și se citesc cuvintele alăturate, pe ecran se afișează
-raita raid raion
-
-raita
-grai
-raid
-raion
-straie
-rai
+Exemplu: dacă textul citit este: testez validez utilizez date corecte acum
+	se obține textul
+testez succes validez succes utilizez date succes corecte acum
 */
