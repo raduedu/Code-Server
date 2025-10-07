155dac04bfc1872eb8b878be3b04b5c229e54749
Edu Radu-Ștefan <eduradustefan1@gmail.com>
2025-10-07 23:53:45 +0300

    test 12 - 2020 - III - 2

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -3,32 +3,37 @@
 #include <cctype>
 using namespace std;
 int main() {
-	int l = 0, ok = 0, lp = 0;
-	char c[101], * p;
+	int l = 0, ok = 0, lp = 0, pozvoc = 0;
+	char c[101], * p, voc[7];
 	cin.getline(c, 101);
+	strcpy(voc, "aeiou");
+	l = strlen(c);
 
-	p = strtok(c, " ");
-	while (p != NULL) {
-		lp = strlen(p);
-		p[0] = toupper(p[0]);
-		for (int i = 1; i < lp; i++)
-			p[i] = tolower(p[i]);
-		//test sync
-		
-		cout << p << " ";
-		
-		p = strtok(NULL, " ");
+	for (int i = 0; i <
+		l; i++) {
+		if (strchr(voc, c[i]) != NULL) {
+			pozvoc = i;
+			ok = 1;
+		}
 	}
+	strcpy(c + pozvoc, c + pozvoc + 1);
+
+	if (ok == 0)
+		cout << ":(";
+	else cout << c;
+	
 
 	return 0;
 }
-/* TEST 11
-Într-un text cu cel mult 10^2 caractere, cuvintele sunt formate din litere mici și mari ale alfabetului englez
-și sunt separate prin câte un spațiu. Scrieți un program C/C++ care citește de la tastatură un text de
-tipul precizat, pe care îl transformă, astfel încât fiecare cuvânt să aibă prima literă mare, și toate
-celelalte litere mici. Textul obținut se afişează pe ecran.
-Exemplu: dacă de la tastatură se introduce textul: 
-ABIA aSTept sa Merg lA scoala
-se obține textul :
-Abia Astept Sa Merg La Scoala
+/* TEST 12
+Într-un text cu cel mult 10^2 caractere, cuvintele sunt formate din litere mici ale alfabetului englez și sunt
+separate prin câte un spațiu. Scrieți un program C/C++ care citește de la tastatură un text de tipul
+precizat, pe care îl transformă în memorie, eliminând numai ultima vocală care apare în text, ca în
+exemplu. Programul afișează pe ecran textul obținut sau mesajul nu exista, dacă în text nu există
+nicio vocală. Se consideră vocale literele a, e, i, o, u.
+Exemplu: dacă se citește textul: 
+cuvantul ritm poate fi tradus rhythm
+
+se obține textul: 
+cuvantul ritm poate fi trads rhythm
 */
