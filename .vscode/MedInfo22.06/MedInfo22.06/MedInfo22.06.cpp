2ed379bc51e87584d9fb3fd1b04f4c9f6ce124eb
Edu Radu-Ștefan <eduradustefan1@gmail.com>
2025-10-08 14:10:14 +0300

    test 18 - 2020 - III - 2

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -1,44 +1,61 @@
-#include <iostream> 
+#include <iostream>
 #include <cstring>
 #include <cctype>
 using namespace std;
-
 int main() {
-	int v = 0, c = 0, l = 0, cnt = 0;
-	char text[101], * p, voc[6];
-	cin.getline(text, 101);
+	int n = 0, k = 0, l = 0, ls = 0, cnt = 0;
+	char s[500], suf[21], cuv[21], * p;
+	cin >> n >> k;
 
-	strcpy(voc, "aeiou");
+	strcpy(s, "");
+	for (int i = 1; i <= n; i++) {
+		cin >> cuv;
 
-	p = strtok(text, " ");
+		if (i == k)
+			strcpy(suf, cuv);
+
+		if (strlen(s) > 0)
+			strcat(s, " ");
+		strcat(s, cuv);
+	}
+
+	ls = strlen(suf);
+
+	p = strtok(s, " ");
 	while (p != NULL) {
-		v = 0;
-		c = 0;
 		l = strlen(p);
 
-		for (int i = 0; i < l; i++) {
-			if (strchr(voc, p[i]) != NULL)
-				v++;
-			else
-				c++;
+		if (l > ls && strcmp(p + l - ls, suf) == 0) {
+			cout << p << " ";
+			cnt = 1;
 		}
-
-		if (v == c)
-			cnt++;
 
 		p = strtok(NULL, " ");
 	}
 
-	cout << cnt;
+	if (cnt == 0)
+		cout << "nu exista";
 
 	return 0;
 }
-/* TEST 15
-Într-un text cu cel mult 100 de caractere, cuvintele sunt formate din litere mici ale alfabetului englez și
-sunt separate prin câte un spațiu. Scrieți un program C/C++ care citește de la tastatură un text de tipul
-menționat și afișează pe ecran numărul de cuvinte ale sale formate dintr-un număr egal de vocale și
-consoane. Se consideră vocale literele din mulțimea a, e, i, o, u.
-Exemplu: pentru textul
-cuvantul consoane are un numar de patru vocale si patru consoane
-se afișează pe ecran 6.
+/* TEST 18
+Un cuvânt este sufix al unui alt cuvânt dacă se obține din acesta, prin eliminarea primelor sale litere.
+Scrieți un program C/C++ care citește de la tastatură două numere naturale n și k (n[2,20],
+k[1,n]) și apoi n cuvinte distincte, fiecare fiind format din cel mult 20 de caractere, numai litere mici
+ale alfabetului englez.
+La introducerea datelor, după fiecare cuvânt se tastează Enter. Programul
+afișează pe ecran, separate prin câte un spațiu, cuvintele care îl au drept sufix
+pe al k-lea cuvânt citit, ca în exemplu. Dacă nu există astfel de cuvinte, se
+afișează pe ecran mesajul nu exista.
+Exemplu: dacă n=7, k=3 și se citesc cuvintele alăturate, pe ecran se afișează
+paratirisi hiritisi
+
+cuv alaturate:
+isihast
+paratirisi
+isi
+meremetisire
+acolisitor
+hiritisi
+paraponisit
 */
