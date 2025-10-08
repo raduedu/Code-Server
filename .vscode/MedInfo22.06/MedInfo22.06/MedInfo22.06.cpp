37c4963493244abeb2b4beec3b6c59c56eda6de0
Edu Radu-Ștefan <eduradustefan1@gmail.com>
2025-10-08 14:02:41 +0300

    test 15 - 2020 - III - 2

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -1,42 +1,44 @@
-#include <iostream>
+#include <iostream> 
 #include <cstring>
 #include <cctype>
 using namespace std;
-int main() {
-	int l = 0, ok = 0;
-	char c[101], * p, rez[500];
-	cin.getline(c, 101);
-	strcpy(rez, "");
 
-	p = strtok(c, " ");
+int main() {
+	int v = 0, c = 0, l = 0, cnt = 0;
+	char text[101], * p, voc[6];
+	cin.getline(text, 101);
+
+	strcpy(voc, "aeiou");
+
+	p = strtok(text, " ");
 	while (p != NULL) {
+		v = 0;
+		c = 0;
 		l = strlen(p);
 
-		if (p[0] == '-') {
-			if (strlen(rez) > 0)
-				strcat(rez, " ");
-			strcat(rez, p + 1);
+		for (int i = 0; i < l; i++) {
+			if (strchr(voc, p[i]) != NULL)
+				v++;
+			else
+				c++;
 		}
-		else {
-			if (strlen(rez) > 0)
-				strcat(rez, " ");
-			strcat(rez, p);
-		}
+
+		if (v == c)
+			cnt++;
 
 		p = strtok(NULL, " ");
 	}
 
-	cout << rez;
+	cout << cnt;
 
 	return 0;
 }
-/* TEST 14
-Un text are cel mult 100 de caractere și este format din cuvinte și numere, separate prin câte un spațiu.
-Cuvintele sunt formate numai din litere ale alfabetului englez. Toate numerele sunt reale și sunt formate
-numai din parte întreagă sau din parte întreagă și parte fracționară, separate prin virgulă (,), numerele
-negative fiind precedate de semnul minus (-). Cel puțin unul dintre numerele reale este negativ. Scrieți
-un program C/C++ care citește de la tastatură textul, pe care îl transformă în memorie, înlocuind fiecare
-număr negativ cu valoarea sa absolută. Programul afișează apoi pe ecran textul obținut.
-Exemplu: pentru textul Modul de -3,24 este 3,24 si modul de -15 este 15
-se va afișa pe ecran textul: Modul de 3,24 este 3,24 si modul de 15 este 15
+/* TEST 15
+Într-un text cu cel mult 100 de caractere, cuvintele sunt formate din litere mici ale alfabetului englez și
+sunt separate prin câte un spațiu. Scrieți un program C/C++ care citește de la tastatură un text de tipul
+menționat și afișează pe ecran numărul de cuvinte ale sale formate dintr-un număr egal de vocale și
+consoane. Se consideră vocale literele din mulțimea a, e, i, o, u.
+Exemplu: pentru textul
+cuvantul consoane are un numar de patru vocale si patru consoane
+se afișează pe ecran 6.
 */
