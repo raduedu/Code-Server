2cef64873402248c9fba3d4eb630d15ae1164356
Edu Radu-Ștefan <eduradustefan1@gmail.com>
2025-10-08 14:11:34 +0300

    test 20 - 2020 - III - 2

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -3,59 +3,38 @@
 #include <cctype>
 using namespace std;
 int main() {
-	int n = 0, k = 0, l = 0, ls = 0, cnt = 0;
-	char s[500], suf[21], cuv[21], * p;
-	cin >> n >> k;
+	int l = 0;
+	char c[101], * p, * virgula, rez[500];
+	cin.getline(c, 101);
 
-	strcpy(s, "");
-	for (int i = 1; i <= n; i++) {
-		cin >> cuv;
+	strcpy(rez, "");
 
-		if (i == k)
-			strcpy(suf, cuv);
-
-		if (strlen(s) > 0)
-			strcat(s, " ");
-		strcat(s, cuv);
-	}
-
-	ls = strlen(suf);
-
-	p = strtok(s, " ");
+	p = strtok(c, " ");
 	while (p != NULL) {
-		l = strlen(p);
+		virgula = strchr(p, ',');
 
-		if (l > ls && strcmp(p + l - ls, suf) == 0) {
-			cout << p << " ";
-			cnt = 1;
-		}
+		if (virgula != NULL)
+			*virgula = '\0';
+
+		if (strlen(rez) > 0)
+			strcat(rez, " ");
+		strcat(rez, p);
 
 		p = strtok(NULL, " ");
 	}
 
-	if (cnt == 0)
-		cout << "nu exista";
+	cout << rez;
 
 	return 0;
 }
-/* TEST 18
-Un cuvânt este sufix al unui alt cuvânt dacă se obține din acesta, prin eliminarea primelor sale litere.
-Scrieți un program C/C++ care citește de la tastatură două numere naturale n și k (n[2,20],
-k[1,n]) și apoi n cuvinte distincte, fiecare fiind format din cel mult 20 de caractere, numai litere mici
-ale alfabetului englez.
-La introducerea datelor, după fiecare cuvânt se tastează Enter. Programul
-afișează pe ecran, separate prin câte un spațiu, cuvintele care îl au drept sufix
-pe al k-lea cuvânt citit, ca în exemplu. Dacă nu există astfel de cuvinte, se
-afișează pe ecran mesajul nu exista.
-Exemplu: dacă n=7, k=3 și se citesc cuvintele alăturate, pe ecran se afișează
-paratirisi hiritisi
-
-cuv alaturate:
-isihast
-paratirisi
-isi
-meremetisire
-acolisitor
-hiritisi
-paraponisit
+/* TEST 20
+Un text cu cel mult 100 de caractere conține cuvinte și numere, separate prin câte un spațiu. Cuvintele
+sunt formate numai din litere mici ale alfabetului englez, iar numerele sunt reale, pozitive, cu partea
+întreagă și partea zecimală separate prin simbolul virgulă, sau numai cu partea întreagă, ca în exemplu.
+Scrieți un program C/C++ care citește de la tastatură un text de tipul precizat și îl transformă în
+memorie, înlocuind fiecare număr real cu partea întreagă a acestuia.
+Exemplu: pentru textul
+partea intreaga a lui 5,75 este egala cu a lui 5,25 si cu a lui 5 si este 5
+se afișează pe ecran
+partea intreaga a lui 5 este egala cu a lui 5 si cu a lui 5 si este 5
 */
