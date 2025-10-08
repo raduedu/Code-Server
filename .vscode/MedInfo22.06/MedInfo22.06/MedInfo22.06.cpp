6805a3d30f01628dfd21e0657536ac74f619e170
Edu Radu-Ștefan <eduradustefan1@gmail.com>
2025-10-08 14:01:41 +0300

    test 14 - 2020 - III - 2

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -3,37 +3,40 @@
 #include <cctype>
 using namespace std;
 int main() {
-	int l = 0, ok = 0, lp = 0, pozvoc = 0;
-	char c[101], * p, voc[7];
+	int l = 0, ok = 0;
+	char c[101], * p, rez[500];
 	cin.getline(c, 101);
-	strcpy(voc, "aeiou");
-	l = strlen(c);
+	strcpy(rez, "");
 
-	for (int i = 0; i <
-		l; i++) {
-		if (strchr(voc, c[i]) != NULL) {
-			pozvoc = i;
-			ok = 1;
+	p = strtok(c, " ");
+	while (p != NULL) {
+		l = strlen(p);
+
+		if (p[0] == '-') {
+			if (strlen(rez) > 0)
+				strcat(rez, " ");
+			strcat(rez, p + 1);
 		}
-	}
-	strcpy(c + pozvoc, c + pozvoc + 1);
+		else {
+			if (strlen(rez) > 0)
+				strcat(rez, " ");
+			strcat(rez, p);
+		}
 
-	if (ok == 0)
-		cout << ":(";
-	else cout << c;
-	
+		p = strtok(NULL, " ");
+	}
+
+	cout << rez;
 
 	return 0;
 }
-/* TEST 12
-Într-un text cu cel mult 10^2 caractere, cuvintele sunt formate din litere mici ale alfabetului englez și sunt
-separate prin câte un spațiu. Scrieți un program C/C++ care citește de la tastatură un text de tipul
-precizat, pe care îl transformă în memorie, eliminând numai ultima vocală care apare în text, ca în
-exemplu. Programul afișează pe ecran textul obținut sau mesajul nu exista, dacă în text nu există
-nicio vocală. Se consideră vocale literele a, e, i, o, u.
-Exemplu: dacă se citește textul: 
-cuvantul ritm poate fi tradus rhythm
-
-se obține textul: 
-cuvantul ritm poate fi trads rhythm
+/* TEST 14
+Un text are cel mult 100 de caractere și este format din cuvinte și numere, separate prin câte un spațiu.
+Cuvintele sunt formate numai din litere ale alfabetului englez. Toate numerele sunt reale și sunt formate
+numai din parte întreagă sau din parte întreagă și parte fracționară, separate prin virgulă (,), numerele
+negative fiind precedate de semnul minus (-). Cel puțin unul dintre numerele reale este negativ. Scrieți
+un program C/C++ care citește de la tastatură textul, pe care îl transformă în memorie, înlocuind fiecare
+număr negativ cu valoarea sa absolută. Programul afișează apoi pe ecran textul obținut.
+Exemplu: pentru textul Modul de -3,24 este 3,24 si modul de -15 este 15
+se va afișa pe ecran textul: Modul de 3,24 este 3,24 si modul de 15 este 15
 */
