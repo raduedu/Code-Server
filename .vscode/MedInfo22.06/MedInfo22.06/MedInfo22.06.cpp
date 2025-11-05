0cbd367fbf7fbf9434a52de42388aa3cc3e84a3b
Edu Radu-Ștefan <eduradustefan1@gmail.com>
2025-11-05 14:21:24 +0200

    Problema 2 - pdf backtracking

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -2,18 +2,37 @@
 using namespace std;
 
 int main() {
-	int n = 0, k = 0, cnt = 0;
-	cin >> n >> k;
+	int k = 0, ext[6], nrext = 0, cnt = 0;
+	cin >> k;
 
-	for (int l = k; l <= n; l++) {
-		int c = 1;
-		for (int i = 1; i <= k - 1; i++) {
-			c = c * (l - i) / i;
-		}
-		cnt += c;
+	for (int i = 1; i <= 10; i++) {
+		if (i < k || i > k + 4)
+			ext[nrext++] = i;
 	}
 
-	cout << cnt;
+	for (int perm = 0; perm < 2 && cnt < 12; perm++) {
+		for (int poz = 0; poz < 6 && cnt < 12; poz++) {
+			for (int i = 0; i < nrext; i++) {
+				if (i == poz) {
+					for (int j = k; j <= k + 4; j++)
+						cout << j << " ";
+				}
+				cout << ext[i] << " ";
+			}
+
+			if (nrext == poz) {
+				for (int j = k; j <= k + 4; j++)
+					cout << j << " ";
+			}
+
+			cout << endl;
+			cnt++;
+		}
+
+		int temp = ext[0];
+		ext[0] = ext[nrext - 1];
+		ext[nrext - 1] = temp;
+	}
 
 	return 0;
 }
