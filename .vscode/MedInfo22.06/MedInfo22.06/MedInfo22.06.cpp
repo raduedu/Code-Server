10a8aa1b468387c5e175efd04e0c02ce5b9bb51b
Edu Radu-Ștefan <eduradustefan1@gmail.com>
2025-11-05 14:14:19 +0200

    Problema 1 - Backtracking pdf

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -1,53 +1,19 @@
 #include <iostream>
-#include <cstring>
-#include <cctype>
 using namespace std;
+
 int main() {
-	int l = 0, cnt = 0, ok = 0;
-	char c[101], * p, voc[6], vocdist[6];
-	cin.getline(c, 101);
+	int n = 0, k = 0, cnt = 0;
+	cin >> n >> k;
 
-	strcpy(voc, "aeiou");
-
-	p = strtok(c, " ");
-	while (p != NULL) {
-		l = strlen(p);
-		strcpy(vocdist, "");
-
-		for (int i = 0; i < l; i++) {
-			if (strchr(voc, p[i]) != NULL) {
-				if (strchr(vocdist, p[i]) == NULL) {
-					char temp[2] = { p[i], '\0' };
-					strcat(vocdist, temp);
-				}
-			}
+	for (int l = k; l <= n; l++) {
+		int c = 1;
+		for (int i = 1; i <= k - 1; i++) {
+			c = c * (l - i) / i;
 		}
-
-		if (strlen(vocdist) == 1) {
-			cout << p << endl;
-			ok = 1;
-		}
-
-		p = strtok(NULL, " ");
+		cnt += c;
 	}
 
-	if (ok == 0)
-		cout << "nu exista";
+	cout << cnt;
 
 	return 0;
 }
-/* TEST 09 - 2021
-Într-un text cu cel mult 102 caractere, cuvintele sunt formate din litere mici ale alfabetului englez și sunt
-separate prin câte un spațiu. Scrieți un program C/C++ care citește de la tastatură un text de tipul precizat și
-afișează pe ecran, pe linii separate, toate cuvintele sale care conțin o singură vocală distinctă, ca în exemplu.
-Dacă nu există niciun astfel de cuvânt, se afișează pe ecran mesajul nu exista. Se
-consideră vocale literele din mulțimea a, e, i, o, u.
-Exemplu: pentru textul a plantat cinci lalele visinii sau rosii
-se afișează pe ecran, nu neapărat în această ordine, cuvintele alăturate.
-
-cuv alaturate:
-a
-plantat
-cinci
-visinii
-*/
