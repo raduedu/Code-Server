0349f55dae6ad7a48b85e8b90ac50ef9b5e55af7
Edu Radu-Ștefan <eduradustefan1@gmail.com>
2025-11-14 15:32:22 +0200

    model 2026 - sub III - 1

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -1,38 +1,25 @@
 #include <iostream>
 using namespace std;
 
+void Plus(int &n) {
+	int x[10], k = 0;
+	while (n > 0) {
+		x[++k] = n % 10;
+		n = n / 10;
+	}
+	for (int i = k; i >= 2; i--) {
+		if (x[i] == 2 && x[i - 1] == 5)
+			x[i - 1] = 6;
+		n = n * 10 + x[i];
+	}
+	n = n * 10 + x[1];
+}
+
 int main() {
-	int k = 0, ext[6], nrext = 0, cnt = 0;
-	cin >> k;
-
-	for (int i = 1; i <= 10; i++) {
-		if (i < k || i > k + 4)
-			ext[nrext++] = i;
-	}
-
-	for (int perm = 0; perm < 2 && cnt < 12; perm++) {
-		for (int poz = 0; poz < 6 && cnt < 12; poz++) {
-			for (int i = 0; i < nrext; i++) {
-				if (i == poz) {
-					for (int j = k; j <= k + 4; j++)
-						cout << j << " ";
-				}
-				cout << ext[i] << " ";
-			}
-
-			if (nrext == poz) {
-				for (int j = k; j <= k + 4; j++)
-					cout << j << " ";
-			}
-
-			cout << endl;
-			cnt++;
-		}
-
-		int temp = ext[0];
-		ext[0] = ext[nrext - 1];
-		ext[nrext - 1] = temp;
-	}
+	int n = 0;
+	cin >> n;
+	Plus(n);
+	cout << endl << n;
 
 	return 0;
 }
