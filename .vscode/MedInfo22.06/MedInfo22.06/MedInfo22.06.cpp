0c8d634fec53425bd1f5bb8af4b17337085ee4dc
Edu Radu-Ștefan <eduradustefan1@gmail.com>
2025-11-19 14:31:08 +0200

    SIII - 3 - model 2025

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main() {
-	int n = 0, a[103][103];
+	long long n = 0, nr2 = 0, nr13 = 0, p = 0;
+	ofstream fout("bac.out");
 	cin >> n;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> a[i][j];
-		}
+	long long d = 2;
+	while (d <= n) {
+		nr2 = nr2 + n / d;
+		d = d * 2;
 	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = n - 1 - i; j < n - 1; j++) {
-			a[i][j] = a[i][j + 1];
-		}
+	d = 13;
+	while (d <= n) {
+		nr13 = nr13 + n / d;
+		d = d * 13;
 	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n - 1; j++) {
-			cout << a[i][j] << " ";
-		}
-		cout << endl;
-	}
+	if (nr2 < nr13)
+		p = nr2;
+	else
+		p = nr13;
+
+	fout << p;
+	fout.close();
 
 	return 0;
 }
