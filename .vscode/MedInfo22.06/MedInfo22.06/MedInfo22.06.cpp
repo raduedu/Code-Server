1f2afba5046449a4dc615362bee3862916b1a82b
Edu Radu-Ștefan <eduradustefan1@gmail.com>
2025-11-20 22:16:49 +0200

    model 2025 - II - 1 - c)

--- .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
+++ .vscode/MedInfo22.06/MedInfo22.06/MedInfo22.06.cpp
@@ -1,34 +1,18 @@
+//model 2025 - II - 1 - c) Scrieti programul in C++
 #include <iostream>
-#include <fstream>
 using namespace std;
 
 int main() {
-	long long n = 0, nr2 = 0, nr13 = 0, p = 0;
-	ofstream fout("bac.out");
-	cin >> n;
+	int m, n, x, y;
+	cin >> m >> n;
+	do {
+		x = m; y = n; n = n - 1;
+		do {
+			if (x > y) x = x - y;
+			else y = y - x;
 
-	long long d = 2;
-	while (d <= n) {
-		nr2 = nr2 + n / d;
-		d = d * 2;
-	}
-
-	d = 13;
-	while (d <= n) {
-		nr13 = nr13 + n / d;
-		d = d * 13;
-	}
-
-	if (nr2 < nr13)
-		p = nr2;
-	else
-		p = nr13;
-
-	fout << p;
-	fout.close();
-
+		} while (!(y == 0));
+	} while (!(x != 1));
+	cout << n + 1;
 	return 0;
 }
-//Programul calculeaza minimul dintre puterea la care apare 2 in descompunerea in factori primi a lui n! si puterea la care apare 13 in factori primi a lui n!
-//Programul este eficient din punct de vedere al memoriei pentru ca stocheaza numarul minim de variabile si nu tine minte n!
-//Programul este eficient din punct de vedere al timpului de executie pentru ca nu il calculeaza pe n!
