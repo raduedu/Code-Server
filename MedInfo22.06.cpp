a36c4f88e4367c6beef70c11005dcac1970783c5
raduedu <eduradustefan1@gmail.com>
2026-04-06 09:58:56 +0300

    2024 - simulare - III.1

--- MedInfo22.06.cpp
+++ MedInfo22.06.cpp
@@ -1,54 +1,27 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
 
-int main(){
-    int max = 0, maxg = 0, nr1 = 0, nr2 = 0, ok, n, aux, a;
-    ifstream fin("bac.in");
-
-    while (fin>>n){
-        max = 0;
-        aux = n;
-        while (aux > 0){
-            a = aux % 10;
-            if (a>max)
-                max = a;
-            aux /=10;
+int Impare(int n){
+    int cn=0, i=1, ok = 0, c = 0;
+    while (n > 0){
+        c = n%10;
+        cn += c*i;
+        if(c%2 == 1){
+            i*=10;
+            cn += c*i;
+            ok = 1;
         }
-        if (max > maxg){
-            maxg = max;
-            nr1 = n;
-            nr2 = 0;
-        }
-        else if (max == maxg)
-            nr2 = n;
-        
+        n /= 10;
+        i*=10;
     }
-    if (nr2 == 0)
-        cout << "Nu exista";
-    else 
-        cout << nr1 << " " <<nr2;
-
-    fin.close();
-    return 0;   
+    if (ok == 1)
+        return cn;
+    else
+        return -1; 
 }
 
-/*
-a)
-Programul citeste fiecare numar din fisier si calculeaza cifra maxima a numarului curent. Daca cifra maxima a numarului 
-curent este mai mare decat cifra maxima globala, actualizeaza maximul global si salveaza numarul curent, 
-resetand al doilea numar la 0. Daca cifra maxima a numarului curent este egala cu maximul global, salveaza numarul curent ca al doilea numar. 
-La final, daca al doilea numar este diferit de 0, inseamna ca exista cel putin doua numere cu cifra maxima globala si se afiseaza primul si ultimul, 
-altfel se afiseaza "Nu exista".
-
-Programul este eficient din punct de vedere al memoriei, deoarece retine doar cifra maxima globala, cifra maxima a numarului curent, 
-primul si ultimul numar si cateva variabile auxiliare. Nu se stocheaza numerele din fisier.
-
-Programul este eficient din punct de vedere al timpului de executie, deoarece fisierul este parcurs o singura data (algoritm liniar), 
-iar pentru fiecare numar se calculeaza cifra maxima in timp. Nu se fac parcurgeri multiple sau sortari, astfel fiind un algoritm liniar.
-*/
-
-
-
-//de facut descriere faina (algoritm liniar) + urmatorul test
-//de gandit pe ce sa insistam la poli
+int main(){
+    int n = 1;
+    cout << Impare(n);
+    return 0;
+}
