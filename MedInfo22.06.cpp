ff5fadb54b4e7a85afdf608ee34365e0926fb6b1
raduedu <eduradustefan1@gmail.com>
2026-04-06 10:55:35 +0300

    2024 - simulare - III.2

--- MedInfo22.06.cpp
+++ MedInfo22.06.cpp
@@ -1,27 +1,41 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-int Impare(int n){
-    int cn=0, i=1, ok = 0, c = 0;
-    while (n > 0){
-        c = n%10;
-        cn += c*i;
-        if(c%2 == 1){
-            i*=10;
-            cn += c*i;
-            ok = 1;
-        }
-        n /= 10;
-        i*=10;
-    }
-    if (ok == 1)
-        return cn;
-    else
-        return -1; 
-}
+int main() {
+	int n, l, ok = 1;
+	char c[101][11], s[101], rez[501], *p;
 
-int main(){
-    int n = 1;
-    cout << Impare(n);
-    return 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> c[i];
+
+	cin.ignore();
+	cin.getline(s, 101);
+
+	p = strtok(s, " ");
+	while (p != NULL) {
+		l = strlen(p);
+		if (p[0] == '*') {
+			ok = 0;
+			for (int i = 1; i <= n; i++)
+				if (strlen(c[i]) == l) {
+					strcat(rez, c[i]);
+					strcat(rez, " ");
+					ok = 1;
+					break;
+				}
+		} else {
+			strcat(rez, p);
+			strcat(rez, " ");
+		}
+		p = strtok(NULL, " ");
+	}
+
+	if (ok == 1)
+		cout << rez;
+	else
+		cout << "imposibil";
+
+	return 0;
 }
