3e3c4d2470f68b282023099ec41dff72a67bb356
raduedu <eduradustefan1@gmail.com>
2026-04-08 09:45:10 +0300

    96 - pbinfo

--- MedInfo22.06.cpp
+++ MedInfo22.06.cpp
@@ -1,33 +1,28 @@
 #include <iostream>
 #include <cstring>
+#include <cctype>
 using namespace std;
 
 int main(){
-    char s[401], rez[401], aux[401], *p; //era 201 inainte, Caught Fatal Signal 11, deci am dublat dimensiunile si acum primesc scorul bun
-    int l, ok = 0;
+    char s[256], rez[21], *p;
+    int l, ok, max = 0;
 
-    cin.getline(s, 201);
-    strcpy(rez, "");
+    cin.getline(s, 256);
 
-    p = strtok(s, " ");
+    p = strtok(s, " :;.,");
     while (p != NULL){
         l = strlen(p);
-        if (l % 2 == 0){
-            ok = 1;
-            strcpy(aux, p + l/2);
-            strncat(aux, p, l/2);
-            strcat(rez, aux);
-        } else {
-            strcat(rez, p);
+        ok = 1;
+        for (int i = 0; i < l/2 && ok == 1; i++)
+            if (p[i] != p[l-1-i] && p[i] != p[l-1-i] + 32 && p[i] != p[l-1-i] - 32)
+                ok = 0;
+        if (ok == 1 && l > max){
+            max = l;
+            strcpy(rez, p);
         }
-        strcat(rez, " ");
-        p = strtok(NULL, " ");
+        p = strtok(NULL, " :;.,");
     }
 
-    if (ok == 1)
-        cout << rez;
-    else
-        cout << "nu exista";
-
+    cout << rez;
     return 0;
 }
